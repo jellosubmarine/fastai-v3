47c0baf5f64113f04abfc5b042003073633abb17
Madis Kaspar Nigol <madis.kaspar.nigol@ut.ee>
2020-05-12 07:42:52 +0300

    some version of sound classification working

--- app/server.py
+++ app/server.py
@@ -8,20 +8,166 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+# Librosa for audio
+import librosa
+# And the display module for visualization
+import librosa.display
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_spectrogram(audio_path):
+    y, sr = librosa.load(audio_path, sr=None)
+    # Let's make and display a mel-scaled power (energy-squared) spectrogram
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+
+    # Convert to log scale (dB). We'll use the peak power (max) as reference.
+    log_S = librosa.power_to_db(S, ref=np.max)
+
+    # Make a new figure
+    plt.figure(figsize=(12, 4))
+
+    # Display the spectrogram on a mel scale
+    # sample rate and hop length parameters are used to render the time axis
+    librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
+
+    # Put a descriptive title on the plot
+    plt.title('mel power spectrogram')
+
+    # draw a color bar
+    plt.colorbar(format='%+02.0f dB')
+
+    # Make the figure layout compact
+    plt.tight_layout()
+
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+def save_spectrogram(audio_fname, image_fname):
+    y, sr = librosa.load(audio_fname, sr=None)
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+    log_S = librosa.power_to_db(S, ref=np.max)
+    librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
+    fig1 = plt.gcf()
+    plt.axis('off')
+    plt.show()
+    plt.draw()
+    fig1.savefig(image_fname, dpi=100)
+
+
+def audio_to_spectrogram(audio_filename):
+    image_fname = audio_filename.split('.')[0] + '.png'
+
+    try:
+        save_spectrogram(audio_filename, image_fname)
+    except ValueError as verr:
+        print('Failed to process %s %s' % (image_fname, verr))
+
+
+# from official code https://colab.research.google.com/drive/1AgPdhSp7ttY18O3fEoHOQKlt_3HJDLi8#scrollTo=cRCaCIb9oguU
+
+
+def _one_sample_positive_class_precisions(scores, truth):
+    """Calculate precisions for each true class for a single sample.
+
+    Args:
+      scores: np.array of (num_classes,) giving the individual classifier scores.
+      truth: np.array of (num_classes,) bools indicating which classes are true.
+
+    Returns:
+      pos_class_indices: np.array of indices of the true classes for this sample.
+      pos_class_precisions: np.array of precisions corresponding to each of those
+        classes.
+    """
+    num_classes = scores.shape[0]
+    pos_class_indices = np.flatnonzero(truth > 0)
+    # Only calculate precisions if there are some true classes.
+    if not len(pos_class_indices):
+        return pos_class_indices, np.zeros(0)
+    # Retrieval list of classes for this sample.
+    retrieved_classes = np.argsort(scores)[::-1]
+    # class_rankings[top_scoring_class_index] == 0 etc.
+    class_rankings = np.zeros(num_classes, dtype=np.int)
+    class_rankings[retrieved_classes] = range(num_classes)
+    # Which of these is a true label?
+    retrieved_class_true = np.zeros(num_classes, dtype=np.bool)
+    retrieved_class_true[class_rankings[pos_class_indices]] = True
+    # Num hits for every truncated retrieval list.
+    retrieved_cumulative_hits = np.cumsum(retrieved_class_true)
+    # Precision of retrieval list truncated at each hit, in order of pos_labels.
+    precision_at_hits = (
+        retrieved_cumulative_hits[class_rankings[pos_class_indices]] /
+        (1 + class_rankings[pos_class_indices].astype(np.float)))
+    return pos_class_indices, precision_at_hits
+
+
+def calculate_per_class_lwlrap(truth, scores):
+    """Calculate label-weighted label-ranking average precision.
+
+    Arguments:
+      truth: np.array of (num_samples, num_classes) giving boolean ground-truth
+        of presence of that class in that sample.
+      scores: np.array of (num_samples, num_classes) giving the classifier-under-
+        test's real-valued score for each class for each sample.
+
+    Returns:
+      per_class_lwlrap: np.array of (num_classes,) giving the lwlrap for each
+        class.
+      weight_per_class: np.array of (num_classes,) giving the prior of each
+        class within the truth labels.  Then the overall unbalanced lwlrap is
+        simply np.sum(per_class_lwlrap * weight_per_class)
+    """
+    assert truth.shape == scores.shape
+    num_samples, num_classes = scores.shape
+    # Space to store a distinct precision value for each class on each sample.
+    # Only the classes that are true for each sample will be filled in.
+    precisions_for_samples_by_classes = np.zeros((num_samples, num_classes))
+    for sample_num in range(num_samples):
+        pos_class_indices, precision_at_hits = (
+            _one_sample_positive_class_precisions(scores[sample_num, :],
+                                                  truth[sample_num, :]))
+        precisions_for_samples_by_classes[sample_num, pos_class_indices] = (
+            precision_at_hits)
+    labels_per_class = np.sum(truth > 0, axis=0)
+    weight_per_class = labels_per_class / float(np.sum(labels_per_class))
+    # Form average of each column, i.e. all the precisions assigned to labels in
+    # a particular class.
+    per_class_lwlrap = (np.sum(precisions_for_samples_by_classes, axis=0) /
+                        np.maximum(1, labels_per_class))
+    # overall_lwlrap = simple average of all the actual per-class, per-sample precisions
+    #                = np.sum(precisions_for_samples_by_classes) / np.sum(precisions_for_samples_by_classes > 0)
+    #           also = weighted mean of per-class lwlraps, weighted by class label prior across samples
+    #                = np.sum(per_class_lwlrap * weight_per_class)
+    return per_class_lwlrap, weight_per_class
+
+
+# Wrapper for fast.ai library
+def lwlrap(scores, truth, **kwargs):
+    score, weight = calculate_per_class_lwlrap(to_np(truth), to_np(scores))
+    return torch.Tensor([(score * weight).sum()])
+
 
 export_file_url = 'https://www.dropbox.com/s/ht4bd2znz8zrlm6/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['basil', 'tomato']
+classes = ['Accelerating_and_revving_and_vroom', 'Accordion', 'Acoustic_guitar', 'Applause', 'Bark', 'Bass_drum', 'Bass_guitar', 'Bathtub_(filling_or_washing)', 'Bicycle_bell', 'Burping_and_eructation', 'Bus', 'Buzz', 'Car_passing_by', 'Cheering', 'Chewing_and_mastication', 'Child_speech_and_kid_speaking', 'Chink_and_clink', 'Chirp_and_tweet', 'Church_bell', 'Clapping', 'Computer_keyboard', 'Crackle', 'Cricket', 'Crowd', 'Cupboard_open_or_close', 'Cutlery_and_silverware', 'Dishes_and_pots_and_pans', 'Drawer_open_or_close', 'Drip', 'Electric_guitar', 'Fart', 'Female_singing', 'Female_speech_and_woman_speaking', 'Fill_(with_liquid)', 'Finger_snapping',
+           'Frying_(food)', 'Gasp', 'Glockenspiel', 'Gong', 'Gurgling', 'Harmonica', 'Hi-hat', 'Hiss', 'Keys_jangling', 'Knock', 'Male_singing', 'Male_speech_and_man_speaking', 'Marimba_and_xylophone', 'Mechanical_fan', 'Meow', 'Microwave_oven', 'Motorcycle', 'Printer', 'Purr', 'Race_car_and_auto_racing', 'Raindrop', 'Run', 'Scissors', 'Screaming', 'Shatter', 'Sigh', 'Sink_(filling_or_washing)', 'Skateboard', 'Slam', 'Sneeze', 'Squeak', 'Stream', 'Strum', 'Tap', 'Tick-tock', 'Toilet_flush', 'Traffic_noise_and_roadway_noise', 'Trickle_and_dribble', 'Walk_and_footsteps', 'Water_tap_and_faucet', 'Waves_and_surf', 'Whispering', 'Writing', 'Yell', 'Zipper_(clothing)']
 path = Path(__file__).parent
 
 app = Starlette()
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.add_middleware(CORSMiddleware, allow_origins=[
+                   '*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
 
 async def download_file(url, dest):
-    if dest.exists(): return
+    if dest.exists():
+        return
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
@@ -57,11 +203,19 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    audio_data = await request.form()
+    audio_bytes = await (audio_data['file'].read())
+    audio_file = wave.open('temp.wav', mode='wb')
+    audio_file.setnchannels(1)
+    audio_file.setframerate(44100.0)
+    audio_file.setsampwidth(2)
+    audio_file.writeframes(audio_bytes)
+    audio_to_spectrogram('temp.wav')
+
+    img = open_image('temp.png')
+    prediction = learn.predict(img, thresh=0.3)
+
+    return JSONResponse({'result': str(prediction[0])})
 
 
 if __name__ == '__main__':
--- app/static/client.js
+++ app/static/client.js
@@ -4,15 +4,15 @@ function showPicker() {
   el("file-input").click();
 }
 
-function showPicked(input) {
-  el("upload-label").innerHTML = input.files[0].name;
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
-  };
-  reader.readAsDataURL(input.files[0]);
-}
+// function showPicked(input) {
+//   el("upload-label").innerHTML = input.files[0].name;
+//   var reader = new FileReader();
+//   reader.onload = function(e) {
+//     el("image-picked").src = e.target.result;
+//     el("image-picked").className = "";
+//   };
+//   reader.readAsDataURL(input.files[0]);
+// }
 
 function analyze() {
   var uploadFiles = el("file-input").files;
@@ -22,14 +22,13 @@ function analyze() {
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
-  xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
+           true);
+  xhr.onerror = function() { alert(xhr.responseText); };
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
+      // el("debug-label").innerHTML = `Debug = ${response["debug"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
@@ -38,4 +37,3 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
